5f231b982e87a6fbf2aa52d5b71b53f45539f3a7
Zachary Rump <zrrm74@mail.missouri.edu>
2017-09-14 18:31:31 -0500

    Still have bug with calculating rs

--- src/masm.c
+++ src/masm.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+
 
 #define BUF_SIZE 128
+
+#define NUM_OPS 3
+#define NUM_REGS 8
+char *OP_STRINGS[] = { "addiu", "bne", "syscall" };
+uint32_t OPCODES[] = { 0x24000000, 0x14000000, 0x0 };
+
+char *REG_STRINGS[] = { "$zero", "$at" "$v0", "$v1", "$a0", "$a1", "$a2", "$a3" };
+uint32_t REGCODES[] = { 0x0, 0x1, 0x2, 0x3, 0x4, 0x5,0x6, 0x7 };
+			// 0 	  1     2      3 
+
 
 int main(int argc, char **argv) {
 //[0x04000000] addiu $a0, $zero, 0 
@@ -23,16 +35,45 @@ int main(int argc, char **argv) {
 	
 	// For each line of input assembly
 	while( fgets(buf, BUF_SIZE, fp_r) != NULL ) {
-		char *tok = NULL;
+		char *op, *rs, *rt, *immediate;
 		// Token on space between instruction and operands
-		tok = strtok(buf, " ");
-		while(tok != NULL) {
-			printf("token: %s\n", tok);
-			tok = strtok(NULL, ",");
-
-		}
-		printf("\n");
+		op = strtok(buf, " ");
+		rs = strtok(NULL, ",");
+		rt = strtok(NULL, ",");
+		immediate = strtok(NULL, "\n");
+		printf("%s %s,%s,%s", op, rs, rt, immediate);
 		
+		uint32_t opc, op1, op2, op3;
+
+		int i;
+		for(i=0; i<NUM_OPS; i++) {
+			if(0 == strcmp(OP_STRINGS[i], op)) {
+				opc = OPCODES[i];
+				//printf("%x\n", opc);
+				break;	
+			}
+		}
+		for(i=0; i<NUM_REGS; i++) { 
+			if(0 == strcmp(rs, REG_STRINGS[i])) {
+				op1 = ( (uint32_t)REGCODES[i] ) << 21;
+				printf("%x\n", op1);
+				break;
+			}
+		}
+		for(i=0; i<NUM_REGS; i++) { 
+			if(0 == strcmp(rt, REG_STRINGS[i])) {
+				op2 = ( (uint32_t)REGCODES[i] ) << 16;
+				//printf("%x\n", op2);
+				break;
+			}
+		}
+		// replace trailing \n with nul terminator
+		//immediate[strlen(immediate)-1] = '\0';
+		uint32_t immediate_int = (uint32_t)strtol(immediate, NULL, 10);
+		
+		//printf("Immediate: %d\n", immediate_int);
+		uint32_t machine_code = opc+op1+op2+immediate_int;
+		printf("machine code: %x\n", machine_code);
 	}
 
 	return EXIT_SUCCESS;
