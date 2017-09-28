1055a7ed1ada394f72233a7503ff6eba475dd4f5
Zachary Rump <zrrm74@mail.missouri.edu>
2017-09-28 17:18:49 -0500

    first branch

--- input/problem2.asm
+++ input/problem2.asm
@@ -1,5 +1,3 @@
-% load integers into memory
-% addr 0x1001000
 addiu $a3,$zero,0x1001
 sll $a0,$a3,10
 addiu $t0,$zero,5
@@ -23,31 +21,27 @@ sw $t0,32($a0)
 addiu $t0,$zero,a
 sw $t0,36($a0)
 
-% $a1 - addr for storing sorted list
-% overwrite old $a3 value we dont need it anymore (addr of unsorted list is in $a0)
-addiu $a3,$zero,0x1002
-sll $a1,$a3,10
-
-% iterate through - same as example from lab document
-% $t1 is upper bound for iterator, $t2 is iterator i
-
-% initial values
-addiu $t1,$zero,0xA
-addiu $t2,$zero,0
-
-%find smallest element in unsorted list 
-% $t3 contains the minimum-element so-far (while we iterate) - initialize to 0
-addiu $t3,$zero,0
-
-
-% increment i
-addiu $t2,$t2,0x01
-
-% Change -8 to the beginning of the finding the smallest element.
-bne $t1,$t2,-8 
+addiu $t0,$zero,0xA 	// n = 10
+addiu $t1,$zero,0x09 	// n-1 = 9
+addiu $t3,$zero,0	// j = 0
+sll $t6,$t3,2		// $t6 = j*4
+add $t7,$a0,$t6		// $t7 = &a[j]
+add $t8,$zero,$t3	// iMin = j
+sll $t9,$t8,2		// $t9 = iMin*4
+add $s0,$a0,$t9		// $s0 = &a[iMin]
+addiu $t2,$t3,1		// i = j+1
+sll $t4,$t2,2		// $t4 = i*4
+add $t5,$a0,$t4		// $t5 = &a[i]
+lw $s2,0($t5)		// $s2 = a[i]
+lw $s3,0($s0)		// $s3 = a[iMin]
+slt $s1,$s2,$s3		// $s1 = (a[i] < a[iMin]) ? 1 : 0
 
 
 
 
 
-syscall
+
+
+
+
+
